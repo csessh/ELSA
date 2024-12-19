04ab553678ab7509b5b681eeeceaea5c4f3ab200
Thang Do <csessh@hey.com>
2024-12-19 23:08:35 +1030

    Create dockerfile, docker compose and other minor dependencies
    notable additional dependency include: redis

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ELSA
 ELSA SPEAK's coding challenge
+
+## Introduction
+
+## Pre-requisite:
+
+The following tools are required:
+
+- Docker
+- Redis
+
+### Redis 
+
+This is a standard Redis installation. It can be done with either Docker or your favourite package manager (Brew, DNF, Snap...):
+
+``` bash
+$ docker run -p 6379:6379 redis/redis-stack:latest
+```
+
+or 
+
+```bash
+$ sudo dnf install redis
+$ sudo systemctl start redis
+```
+
+Once installed, validate with `redis-cli`:
+
+``` bash
+$ redis-cli ping
+PONG
+```
+
+## Server 
+
+
+## Client simulation
---
+++ cli.py
@@ -0,0 +1,18 @@
+from rich import print
+from argparse import ArgumentParser, Namespace
+from client.user import ElsaClient
+
+
+def main(args: Namespace):
+    print(args)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(prog="Elsa", description="Quiz Client", epilog="Let it go, let it go")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-s", "--simulation", action="store_true", help="Enable simulation mode")
+    group.add_argument("-i", "--interactive", action="store_true", help="Enable interactive mode")
+    parser.add_argument("-v", "--verbose", action="store_true", help="")
+
+    args = parser.parse_args()
+    main(args)
